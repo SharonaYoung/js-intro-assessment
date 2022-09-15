0951b8db1ac5ff18339806db5bcc5c9b35300756
Sharona Young <msrona806@gmail.com>
2022-09-15 18:17:31 -0400

    added values to variables and alerts

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 let firstName;
 let lastName;
 let age;
+
+firstName = "Sharona";
+lastName = "Young";
+age = 41;
+
+alert ("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+
+console.log("I am enjoying this class, can't wait to learn more!");
